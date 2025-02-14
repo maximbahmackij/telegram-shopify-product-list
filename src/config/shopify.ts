35d7d47ac866a1f73dd0e4985a4ca28b87792c10
Maksim Bahmackij <maxim.bahmackij@gmail.com>
2025-02-14 18:17:55 +0200

    add shopify api version variable

--- src/config/shopify.ts
+++ src/config/shopify.ts
@@ -2,7 +2,7 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 
 const client = createStorefrontApiClient({
   storeDomain: process.env.SHOPIFY_STORE_URL!,
-  apiVersion: "2025-01",
+  apiVersion: process.env.SHOPIFY_API_VERSION ?? "2025-01",
   publicAccessToken: process.env.SHOPIFY_STOREFRONT_TOKEN!,
 });
 
